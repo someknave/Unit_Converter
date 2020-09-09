fec97ae866ea09e1693c646a4a918bb053ec2f10
Joe Levey <josephfalevey@gmail.com>
2020-09-10 04:19:49 +1000

    complete Stage 2

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,7 +1,11 @@
 package converter
 
+import java.util.*
+val input = Scanner(System.`in`)
+
 fun main() {
-    printConversions()
+    kmToMeters()
+
 }
 
 fun printConversions() {
@@ -10,4 +14,9 @@ fun printConversions() {
     println("5.5 inches is 139.7 millimeters")
     println("12 degrees Celsius is 53.6 degrees Fahrenheit")
     println("3 pounds is 1.360776 kilograms")
+}
+fun kmToMeters() {
+    print("Enter a number of kilometers:")
+    val km = input.nextInt()
+    println("$km kilometers is ${km * 1000} meters")
 }
