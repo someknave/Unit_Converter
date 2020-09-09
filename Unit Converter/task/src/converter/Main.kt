dad8f4f147c6932c40413d73e57db7fbec61ffa5
Joe Levey <josephfalevey@gmail.com>
2020-09-10 04:01:44 +1000

    Initial Commit

---
+++ Unit Converter/task/src/converter/Main.kt
@@ -0,0 +1,13 @@
+package converter
+
+fun main() {
+    printConversions()
+}
+
+fun printConversions() {
+    println("145 centimeters is 1.45 meters")
+    println("2 miles is 3.2187 kilometers")
+    println("5.5 inches is 139.7 millimeters")
+    println("12 degrees Celsius is 53.6 degrees Fahrenheit")
+    println("3 pounds is 1.360776 kilograms")
+}
