0a68b3fd016141b5bbcf6eb4fb7a13598bd10b33
Joe Levey <josephfalevey@gmail.com>
2020-09-10 06:21:19 +1000

    Complete stage 3

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -2,9 +2,30 @@ package converter
 
 import java.util.*
 val input = Scanner(System.`in`)
+enum class UnitLength(val unit: String = "", val singular: String = "",
+                      val plural: String = "", val toMeter: Double = 0.0) {
+    METER("m", "meter", "meters", 1.0),
+    KILOMETER("km", "kilometer", "kilometers", 1000.0),
+    CENTIMETER("cm", "centimeter", "centimeters", 0.01),
+    MILLIMETER("mm", "millimeter", "millimeters", 0.001),
+    MILE("mi", "mile", "miles", 1609.35),
+    YARD("yd", "yard", "yards", 0.9144),
+    FOOT("ft", "foot", "feet", 0.3048),
+    INCH("in", "inch", "inches", 0.0254);
+
+}
+fun getUnitLength(string: String): UnitLength? {
+    for (unit in UnitLength.values()) {
+        if (string in listOf(unit.unit, unit.singular, unit.plural)){
+            return unit
+        }
+    }
+    return null
+}
+
 
 fun main() {
-    kmToMeters()
+    convertToMeter()
 
 }
 
@@ -19,4 +40,23 @@ fun kmToMeters() {
     print("Enter a number of kilometers:")
     val km = input.nextInt()
     println("$km kilometers is ${km * 1000} meters")
+}
+
+fun convertToMeter() {
+    print("Enter a number and a measure of length:")
+    val amount = input.nextDouble()
+    val unit = input.next().toLowerCase()
+    val unitLength = getUnitLength(unit)
+    if(unitLength == null) {
+        println("Invalid Unit")
+        return
+    }
+    val output = amount.toDouble() * unitLength.toMeter
+    val meterForm = if (output == 1.0) {
+        UnitLength.METER.singular
+    } else UnitLength.METER.plural
+    val unitForm = if (amount == 1.0) {
+        unitLength.singular
+    } else unitLength.plural
+    println("$amount $unitForm is $output $meterForm")
 }
