edce52d6cff32d2a312ecd694adfaec64f982c9e
Joe Levey <josephfalevey@gmail.com>
2020-09-11 14:11:43 +1000

    completed build

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -2,31 +2,51 @@ package converter
 
 import java.util.*
 val input = Scanner(System.`in`)
-enum class UnitLength(val unit: String = "", val singular: String = "",
-                      val plural: String = "", val toMeter: Double = 0.0) {
-    METER("m", "meter", "meters", 1.0),
-    KILOMETER("km", "kilometer", "kilometers", 1000.0),
-    CENTIMETER("cm", "centimeter", "centimeters", 0.01),
-    MILLIMETER("mm", "millimeter", "millimeters", 0.001),
-    MILE("mi", "mile", "miles", 1609.35),
-    YARD("yd", "yard", "yards", 0.9144),
-    FOOT("ft", "foot", "feet", 0.3048),
-    INCH("in", "inch", "inches", 0.0254);
+
+
+enum class Units(val singular: String = "",
+                      val plural: String = "", val unitType: String = "",
+                 val toStandardMult: Double = 0.0, val toStandardAdd: Double = 0.0, val aliases: List<String> = emptyList()) {
+    METER( "meter", "meters", "length", 1.0, 0.0, listOf("meter", "meters", "m")),
+    KILOMETER( "kilometer", "kilometers", "length",1000.00,.0, listOf("kilometer", "kilometers", "km")),
+    CENTIMETER( "centimeter", "centimeters", "length",0.010,.0, listOf("centimeter", "centimeters", "cm")),
+    MILLIMETER("millimeter", "millimeters", "length",0.001,0.0, listOf("millimeter", "millimeters", "mm")),
+    MILE( "mile", "miles", "length",1609.35,0.0, listOf("mile", "miles", "mi")),
+    YARD( "yard", "yards", "length",0.9144,0.0, listOf("yard", "yards", "yd")),
+    FOOT( "foot", "feet", "length",0.3048,0.0, listOf("foot", "feet", "ft")),
+    INCH( "inch", "inches", "length",0.0254,0.0, listOf("inch", "inches", "in")),
+    GRAM( "gram", "grams", "weight", 1.0,0.0, listOf("gram", "grams", "g")),
+    KILOGRAM("kilogram", "kilograms", "weight", 1000.0,0.0, listOf("kilogram", "kilograms", "kg")),
+    MILLIGRAM( "milligram", "milligrams", "weight", 0.001,0.0, listOf("milligram", "milligrams", "mg")),
+    POUND( "pound", "pounds", "weight", 453.592,0.0, listOf("pound", "pounds", "lb")),
+    OUNCE("ounce", "ounces", "weight", 28.3495,0.0, listOf("ounce", "ounces", "oz")),
+    CELSIUS("degree Celsius", "degrees Celsius", "temperature", 1.0, 0.0, listOf("c", "dc", "celsius")),
+    FAHRENHEIT("degree Fahrenheit", "degrees Fahrenheit", "temperature", 5.0/9.0, -32.0, listOf("f", "df", "fahrenheit")),
+    KELVIN("kelvin", "kelvins", "temperature", 1.0, -273.15, listOf("kelvin", "kelvins", "k")),
+    DERPDERP("derp");
 
 }
-fun getUnitLength(string: String): UnitLength? {
-    for (unit in UnitLength.values()) {
-        if (string in listOf(unit.unit, unit.singular, unit.plural)){
+
+fun getUnit(string: String): Units {
+    for (unit in Units.values()) {
+        if (string in unit.aliases) {
             return unit
         }
     }
-    return null
+    return Units.DERPDERP
 }
 
 
-fun main() {
-    convertToMeter()
 
+fun main() {
+    while (true) {
+        println("Enter what you want to convert (or exit):")
+        var list = input.nextLine().split(Regex("\\s+"))
+        list = list.map { it.toLowerCase() }
+        if (list[0] == "exit") break
+        list = list.minus(listOf("", "degree", "degrees"))
+        convertToUnit(list)
+    }
 }
 
 fun printConversions() {
@@ -42,21 +62,32 @@ fun kmToMeters() {
     println("$km kilometers is ${km * 1000} meters")
 }
 
-fun convertToMeter() {
-    print("Enter a number and a measure of length:")
-    val amount = input.nextDouble()
-    val unit = input.next().toLowerCase()
-    val unitLength = getUnitLength(unit)
-    if(unitLength == null) {
-        println("Invalid Unit")
-        return
+
+
+fun convertToUnit(args: List<String>) {
+    val input  = args[0].toDoubleOrNull()?: return println("Parse error")
+    if (args.size != 4) return println("Parse error")
+    val initUnit = getUnit(args[1])
+    val targetUnit = getUnit(args[3])
+    when {
+        initUnit == Units.DERPDERP && targetUnit == Units.DERPDERP -> return println("Conversion from ??? to ??? is impossible.")
+        initUnit == Units.DERPDERP -> return println("Conversion from ??? to ${targetUnit.plural} is impossible.")
+        targetUnit == Units.DERPDERP -> return println("Conversion from ${initUnit.plural} to ??? is impossible.")
     }
-    val output = amount.toDouble() * unitLength.toMeter
-    val meterForm = if (output == 1.0) {
-        UnitLength.METER.singular
-    } else UnitLength.METER.plural
-    val unitForm = if (amount == 1.0) {
-        unitLength.singular
-    } else unitLength.plural
-    println("$amount $unitForm is $output $meterForm")
+    if (initUnit.unitType != targetUnit.unitType) {
+        return println("Conversion from ${ initUnit.plural } to ${ targetUnit.plural } is impossible.")
+    }
+    val ans = when {
+        initUnit.unitType == "temperature" -> {
+            (input + initUnit.toStandardAdd) * initUnit.toStandardMult / targetUnit.toStandardMult - targetUnit.toStandardAdd
+        }
+        input < 0 -> return println("${initUnit.unitType.capitalize()} shouldn't be negative.")
+        else -> input * initUnit.toStandardMult / targetUnit.toStandardMult
+    }
+    val initName = if (input == 1.0) { initUnit.singular
+    } else { initUnit.plural}
+    val targetName = if (ans == 1.0) { targetUnit.singular
+    } else { targetUnit.plural}
+    println("$input $initName is $ans $targetName")
 }
+
